094c045a6e647fea7a912a870043b5bcbb0724c9
Rehan Ahmed <Rehan.ahmed19@outlook.com>
2016-07-19 21:29:41 +0500

    Add jquery to sticky navbar

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+
+	/* For sticky navbar */
+	$('.js--section-features').waypoint(function(direction){
+		if (direction == "down") {
+			$('nav').addClass("sticky");
+		}else{
+			$('nav').removeClass("sticky");
+		}
+	}, {
+		offset: "60px"
+	});
+
+
+	/* Scroll on buttons */
 	
 });
